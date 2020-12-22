11649d50173dbe7158f87c2cea7c9d255ae40057
yeseol-lee <1999onell@gmail.com>
2020-12-22 15:39:13 +0900

    class

---
+++ 18.instance methods2.js
@@ -0,0 +1,16 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    static distance(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+        
+        return Maht.hypot(dx, dy);
+    }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
